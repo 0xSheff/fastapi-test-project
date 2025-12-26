262461ba72330f21cb32413cfc111f54fbbb1250
0xsheff <0xsheff@gmail.com>
2025-12-26 12:09:24 +0000

    add nginx

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ DC = docker compose
 PHONY: up
 
 up:
-	${DC} up -d
+	${DC} up -d --remove-orphans
 
 down:
 	${DC} down
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,10 @@
+import socket
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/api",
+)
+
+@app.get("/info")
+async def get_backend():
+    return {"backend": socket.gethostname()}
