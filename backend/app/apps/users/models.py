fc47eb58b9b6906fc3c53c5484e5a6b45523d82e
0xsheff <0xsheff@gmail.com>
2026-02-01 18:13:08 +0200

    added admin field to user

--- backend/app/apps/users/models.py
+++ backend/app/apps/users/models.py
@@ -6,3 +6,4 @@ class User(Base):
     name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[str]
+    is_admin: Mapped[bool] = mapped_column(default=False, nullable=True)
--- backend/app/apps/users/router.py
+++ backend/app/apps/users/router.py
@@ -1,9 +1,9 @@
-from apps.auth.dependencies import get_current_user
+from apps.auth.dependencies import get_admin_user, get_current_user
 from apps.core.dependencies import get_async_session
 from apps.users.crud import user_manager
 from apps.users.models import User
 from apps.users.schemas import RegisteredUserSchema, RegisterUserSchema
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 users_router = APIRouter()
@@ -19,4 +19,20 @@ async def create_user(
 
 @users_router.get("/user-info")
 async def get_my_info(user: User = Depends(get_current_user)) -> RegisteredUserSchema:
+    return RegisteredUserSchema.model_validate(user)
+
+
+@users_router.get("/{id}", dependencies=[Depends(get_admin_user)])
+async def get_user(
+    user_id: int = Path(..., description="The id of the user", ge=1, alias="id"),
+    session: AsyncSession = Depends(get_async_session),
+) -> RegisteredUserSchema:
+    user: User | None = await user_manager.get(
+        session=session, field_value=user_id, field=User.id
+    )
+    if not user:
+        raise HTTPException(
+            detail="User with given email not found",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     return RegisteredUserSchema.model_validate(user)
